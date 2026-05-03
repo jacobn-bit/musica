6a0b2d8460831ca505233facc9c54138573bb948
jacobn-bit <jacobniazov@gmail.com>
2026-05-03 15:18:40 +0300

    Delete Musica final directory

--- Musica final/config.js
+++
@@ -1,4 +0,0 @@
-window.MUSICA_CONFIG = {
-  SUPABASE_URL: "https://cwdbhzpdqxctssnhuqcp.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3ZGJoenBkcXhjdHNzbmh1cWNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3Njk4MjQsImV4cCI6MjA5MzM0NTgyNH0.VK258257sA1MkQ34SLMsVXYxSPliYzkQS9AvH8FZ-ks"
-};
