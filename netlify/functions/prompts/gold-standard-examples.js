fca196b15f3d824691d5c53797e6891aeab39585
jacobn-bit <jacobniazov@gmail.com>
2026-08-11 17:32:28 +0300

    Create gold-standard-examples.js

---
+++ netlify/functions/prompts/gold-standard-examples.js
@@ -0,0 +1,5 @@
+"use strict";
+
+// Intentionally empty. Approved examples are loaded from muze_editorial_examples.
+// This file is the safe version-controlled fallback and must not contain invented reviews.
+module.exports = Object.freeze([]);
